8915754d602783c85a834ccca6a78154504c8130
Humberto Rivero <63797901+danieldamian09@users.noreply.github.com>
2022-11-21 22:31:50 -0300

    Add test en component Counter

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import {useState} from "react";
-import reactLogo from "../../assets/react.svg";
 
 export interface CounterInterface {
 	value: number;
@@ -10,11 +9,8 @@ const Counter: React.FC<CounterInterface> = ({value}) => {
 	const [count, setCount] = useState(value);
 	return (
 		<div className="App">
-			<div>
-				<img src={reactLogo} className="logo react" alt="React logo" />
-			</div>
 			<h1>Testing React</h1>
-			<p>{count}</p>
+			<h2 data-aria="number">{count}</h2>
 			<div className="card">
 				<button onClick={() => setCount((count) => count + 1)}>+1</button>
 				<button onClick={() => setCount((count) => count - 1)}>-1</button>
