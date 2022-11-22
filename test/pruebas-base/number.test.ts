035f04f7db46c0016abd88f679882b7797fee488
danieldamian09 <humbertorivero9@gmail.com>
2022-11-22 17:53:07 -0300

    Fix test clase

--- test/pruebas-base/number.test.ts
+++
@@ -1,8 +0,0 @@
-import {suma} from "../../src/pruebas-base/number";
-describe("Pruebas en number.ts", () => {
-	test("Debe retornar la suma de dos numeros", () => {
-		const result = suma(1, 2);
-    expect(result).toBe(3);
-	});
-	
-});
--- test/pruebas-base/string.test.ts
+++
@@ -1,9 +0,0 @@
-import { getSaludo } from '../../src/pruebas-base/string';
-describe('Pruebas en string.ts', () => {
-    test('getSaludo debe retornar Hola + nombre', () => {
-        const nombre = 'Humberto';
-        const saludo = getSaludo(nombre);
-        expect(saludo).toBe('Hola ' + nombre);
-    });
-
-})
